74b3d0a66729568199f4c8bccb38ad295067cb52
BadduCoder <sumit7engineer@gmail.com>
2021-04-25 23:27:04 +0530

    Added dynamic filter/cards

--- views/requirement_view/requirementsData.js
+++ views/requirement_view/requirementsData.js
@@ -73,4 +73,82 @@ const requirementData = [
     }
 ];
 
+const bloodRequirementData = [
+    {
+        'id' : '1',
+        'entityName' : "Sumit Badsara",
+        'requirementName' : "O+",
+        'plasma':true,
+        'blood':false,
+        'primaryContact' : '+91 XXXXXXXXXX',
+        'secondaryContact' : '+91 XXXXXXXXXX',
+        'location' : "Jaipur, Rajasthan",
+        'verified' : true,
+    },
+    {
+        'id' : '2',
+        'entityName' : "Apollo Hospital",
+        'requirementName' : "Oxygen",
+        'primaryContact' : '+91 XXXXXXXXXX',
+        'secondaryContact' : '+91 XXXXXXXXXX',
+        'location' : "Jaipur, Rajasthan",
+        'verified' : false,
+    },
+    {
+        'id' : '3',
+        'entityName' : "Apollo Hospital",
+        'requirementName' : "Oxygen",
+        'primaryContact' : '+91 XXXXXXXXXX',
+        'secondaryContact' : '+91 XXXXXXXXXX',
+        'location' : "Jaipur, Rajasthan",
+        'verified' : true, 
+    },
+    {
+        'id' : '4',
+        'entityName' : "Apollo Hospital",
+        'requirementName' : "Oxygen",
+        'primaryContact' : '+91 XXXXXXXXXX',
+        'secondaryContact' : '+91 XXXXXXXXXX',
+        'location' : "Jaipur, Rajasthan",
+        'verified' : true, 
+    },
+    {
+        'id' : '5',
+        'entityName' : "Apollo Hospital",
+        'requirementName' : "Oxygen",
+        'primaryContact' : '+91 XXXXXXXXXX',
+        'secondaryContact' : '+91 XXXXXXXXXX',
+        'location' : "Jaipur, Rajasthan",
+        'verified' : true, 
+    },
+    {
+        'id' : '6',
+        'entityName' : "Apollo Hospital",
+        'requirementName' : "Oxygen",
+        'primaryContact' : '+91 XXXXXXXXXX',
+        'secondaryContact' : '+91 XXXXXXXXXX',
+        'location' : "Jaipur, Rajasthan",
+        'verified' : true, 
+    },
+    {
+        'id' : '7',
+        'entityName' : "Apollo Hospital",
+        'requirementName' : "Oxygen",
+        'primaryContact' : '+91 XXXXXXXXXX',
+        'secondaryContact' : '+91 XXXXXXXXXX',
+        'location' : "Jaipur, Rajasthan",
+        'verified' : false, 
+    },
+    {
+        'id' : '8',
+        'entityName' : "Apollo Hospital",
+        'requirementName' : "Oxygen",
+        'primaryContact' : '+91 XXXXXXXXXX',
+        'secondaryContact' : '+91 XXXXXXXXXX',
+        'location' : "Jaipur, Rajasthan",
+        'verified' : true, 
+    }
+];
+
+
 export default requirementData;
